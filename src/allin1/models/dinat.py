4276400ea0beb8ef2879c4cfad45e3a3555a33b9
Tony <github@tony-pappas.com>
2026-02-18 11:56:46 -0700

    Add natten functional imports and update attention methods

--- src/allin1/models/dinat.py
+++ src/allin1/models/dinat.py
@@ -7,6 +7,7 @@ import math
 import torch
 from abc import ABC,  abstractmethod
 from typing import Optional, Tuple, Callable
+from natten.functional import natten1dav, natten1dqkrpb, natten2dav, natten2dqkrpb
 from natten.functional import na1d_av, na1d_qk, na2d_av, na2d_qk
 from ..config import Config
 from .utils import *
@@ -36,14 +37,14 @@ def drop_path(input, drop_prob=0.0, training=False, scale_by_keep=True):
 # Copied from transformers.models.beit.modeling_beit.BeitDropPath with Beit->Dinat
 class DinatDropPath(nn.Module):
   """Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks)."""
-  
+
   def __init__(self, drop_prob: Optional[float] = None) -> None:
     super().__init__()
     self.drop_prob = drop_prob
-  
+
   def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
     return drop_path(hidden_states, self.drop_prob, self.training)
-  
+
   def extra_repr(self) -> str:
     return "p={}".format(self.drop_prob)
 
@@ -52,8 +53,10 @@ class _NeighborhoodAttentionNd(ABC, nn.Module):
   # rpb is learnable relative positional biases; same concept is used Swin.
   rpb: nn.Parameter
   nattendqkrpb: Callable
+  na1d_qk: Callable
   nattendav: Callable
   
+
   def __init__(
     self,
     cfg: Config,
@@ -67,19 +70,19 @@ class _NeighborhoodAttentionNd(ABC, nn.Module):
       raise ValueError(
         f"The hidden size ({dim}) is not a multiple of the number of attention heads ({num_heads})"
       )
-    
+
     self.num_attention_heads = num_heads
     self.attention_head_size = int(dim / num_heads)
     self.all_head_size = self.num_attention_heads * self.attention_head_size
     self.kernel_size = kernel_size
     self.dilation = dilation
-    
+
     self.query = nn.Linear(self.all_head_size, self.all_head_size, bias=cfg.qkv_bias)
     self.key = nn.Linear(self.all_head_size, self.all_head_size, bias=cfg.qkv_bias)
     self.value = nn.Linear(self.all_head_size, self.all_head_size, bias=cfg.qkv_bias)
-    
+
     self.dropout = nn.Dropout(cfg.drop_attention)
-  
+
   def forward(
     self,
     hidden_states: torch.Tensor,
@@ -88,23 +91,25 @@ class _NeighborhoodAttentionNd(ABC, nn.Module):
     query_layer = self.transpose_for_scores(self.query(hidden_states))
     key_layer = self.transpose_for_scores(self.key(hidden_states))
     value_layer = self.transpose_for_scores(self.value(hidden_states))
-    
+
     # Apply the scale factor before computing attention weights. It's usually more efficient because
     # attention weights are typically a bigger tensor compared to query.
     # It gives identical results because scalars are commutable in matrix multiplication.
     query_layer = query_layer / math.sqrt(self.attention_head_size)
-    
+
     # Compute NA between "query" and "key" to get the raw attention scores, and add relative positional biases.
     # attention_scores = natten2dqkrpb(query_layer, key_layer, self.rpb, self.dilation)
     attention_scores = self.nattendqkrpb(query_layer, key_layer, self.rpb, self.kernel_size, self.dilation)
     
+    attention_scores = self.na_qk(query_layer, key_layer,  self.kernel_size, self.dilation, rpb=self.rpb)
+
     # Normalize the attention scores to probabilities.
     attention_probs = nn.functional.softmax(attention_scores, dim=-1)
-    
+
     # This is actually dropping out entire tokens to attend to, which might
     # seem a bit unusual, but is taken from the original Transformer paper.
     attention_probs = self.dropout(attention_probs)
-    
+
     # context_layer = natten2dav(attention_probs, value_layer, self.dilation)
     context_layer = self.nattendav(attention_probs, value_layer, self.kernel_size, self.dilation)
     if len(context_layer.shape) > 4:  # 2D
@@ -113,11 +118,11 @@ class _NeighborhoodAttentionNd(ABC, nn.Module):
       context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
     new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
     context_layer = context_layer.view(new_context_layer_shape)
-    
+
     outputs = (context_layer, attention_probs) if output_attentions else (context_layer,)
-    
+
     return outputs
-  
+
   def transpose_for_scores(self, x):
     new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
     x = x.view(new_x_shape)
@@ -143,6 +148,8 @@ class NeighborhoodAttention1d(_NeighborhoodAttentionNd):
     )
     self.nattendqkrpb = natten1dqkrpb
     self.nattendav = natten1dav
+    self.na_qk = na1d_qk
+    self.nattendav = na1d_av
 
 
 class NeighborhoodAttention2d(_NeighborhoodAttentionNd):
@@ -161,6 +168,8 @@ class NeighborhoodAttention2d(_NeighborhoodAttentionNd):
     )
     self.nattendqkrpb = natten2dqkrpb
     self.nattendav = natten2dav
+    self.na_qk = na2d_qk
+    self.nattendav = na2d_av
 
 
 # Copied from transformers.models.nat.modeling_nat.NeighborhoodAttentionOutput
@@ -169,22 +178,22 @@ class NeighborhoodAttentionOutput(nn.Module):
     super().__init__()
     self.dense = nn.Linear(dim, dim)
     self.dropout = nn.Dropout(config.drop_attention)
-  
+
   def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
     hidden_states = self.dense(hidden_states)
     hidden_states = self.dropout(hidden_states)
-    
+
     return hidden_states
 
 
 class _NeighborhoodAttentionModuleNd(ABC, nn.Module):
   self: _NeighborhoodAttentionNd
-  
+
   def __init__(self, cfg: Config, dim: int):
     super().__init__()
     # self.self = _NeighborhoodAttentionNd(config, dim, num_heads, kernel_size, dilation)
     self.output = NeighborhoodAttentionOutput(cfg, dim)
-  
+
   def forward(
     self,
     hidden_states: torch.Tensor,
@@ -217,7 +226,7 @@ class DinatIntermediate(nn.Module):
       self.intermediate_act_fn = get_activation_function(config.act_transformer)
     else:
       self.intermediate_act_fn = config.act_transformer
-  
+
   def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
     hidden_states = self.dense(hidden_states)
     hidden_states = self.intermediate_act_fn(hidden_states)
@@ -230,7 +239,7 @@ class DinatOutput(nn.Module):
     super().__init__()
     self.dense = nn.Linear(dim_in, dim_out)
     self.dropout = nn.Dropout(config.drop_hidden)
-  
+
   def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
     hidden_states = self.dense(hidden_states)
     hidden_states = self.dropout(hidden_states)
@@ -240,7 +249,7 @@ class DinatOutput(nn.Module):
 class _DinatLayerNd(ABC, nn.Module):
   attention: _NeighborhoodAttentionModuleNd
   attention2: Optional[_NeighborhoodAttentionModuleNd]
-  
+
   def __init__(
     self,
     cfg: Config,
@@ -263,11 +272,11 @@ class _DinatLayerNd(ABC, nn.Module):
     self.layernorm_after = nn.LayerNorm(dim_after, eps=cfg.layer_norm_eps)
     self.intermediate = DinatIntermediate(cfg, dim_after, int(dim_after * cfg.mlp_ratio))
     self.output = DinatOutput(cfg, int(dim_after * cfg.mlp_ratio), dim)
-  
+
   @abstractmethod
   def maybe_pad(self, *args, **kwargs):
     raise NotImplementedError
-  
+
   def forward(
     self,
     hidden_states: torch.Tensor,
@@ -280,7 +289,7 @@ class _DinatLayerNd(ABC, nn.Module):
       is_2d = False
       N, T, C = hidden_states.shape
     shortcut = hidden_states
-    
+
     hidden_states = self.layernorm_before(hidden_states)
     # pad hidden_states if they are smaller than kernel size x dilation
     if is_2d:
@@ -288,16 +297,16 @@ class _DinatLayerNd(ABC, nn.Module):
       _, height_pad, width_pad, _ = hidden_states.shape
     else:
       hidden_states, pad_values = self.maybe_pad(hidden_states, T)
-    
+
     attention_inputs = hidden_states
     hidden_states_list = []
     for attention in [self.attention, self.attention2]:
       if attention is None:
         continue
-      
+
       attention_output = attention(attention_inputs, output_attentions=output_attentions)
       attention_output = attention_output[0]
-      
+
       if is_2d:
         was_padded = pad_values[3] > 0 or pad_values[5] > 0
         if was_padded:
@@ -306,10 +315,10 @@ class _DinatLayerNd(ABC, nn.Module):
         was_padded = pad_values[3] > 0
         if was_padded:
           attention_output = attention_output[:, :T, :].contiguous()
-      
+
       hidden_states = shortcut + self.drop_path(attention_output)
       hidden_states_list.append(hidden_states)
-    
+
     if self.double_attention:
       hidden_states = torch.cat(hidden_states_list, dim=-1)
       shortcut = torch.stack(hidden_states_list).sum(dim=0) / 2.
@@ -317,9 +326,9 @@ class _DinatLayerNd(ABC, nn.Module):
       shortcut = hidden_states
     layer_output = self.layernorm_after(hidden_states)
     layer_output = self.output(self.intermediate(layer_output))
-    
+
     layer_output = shortcut + self.drop_path(layer_output)
-    
+
     # layer_outputs = (layer_output, attention_outputs[1]) if output_attentions else (layer_output,)
     layer_outputs = (layer_output,)
     return layer_outputs
@@ -342,7 +351,7 @@ class DinatLayer1d(_DinatLayerNd):
       self.attention2 = NeighborhoodAttentionModule1d(cfg, dim, num_heads, kernel_size, dilation * 2)
     else:
       self.attention2 = None
-  
+
   def maybe_pad(self, hidden_states, frames):
     window_size = self.window_size
     pad_values = (0, 0, 0, 0)
@@ -367,7 +376,7 @@ class DinatLayer2d(_DinatLayerNd):
     super().__init__(cfg, dim, kernel_size, dilation, drop_path_rate, double_attention=False)
     self.attention = NeighborhoodAttentionModule2d(cfg, dim, num_heads, kernel_size, dilation)
     self.attention2 = None
-  
+
   def maybe_pad(self, hidden_states, height, width):
     window_size = self.window_size
     pad_values = (0, 0, 0, 0, 0, 0)
